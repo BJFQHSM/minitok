2ca3f7d7327e9c8d5d2b6dfab429f4bbb50ba599
mango19970707 <105163203+mango19970707@users.noreply.github.com>
2022-06-03 18:00:03 +0800

    Update favorite_action.go

--- cmd/biz/service/favorite_action.go
+++ cmd/biz/service/favorite_action.go
@@ -62,16 +62,15 @@ func (s *favoriteActionServiceImpl) validateParams() error {
 }
 
 func (s *favoriteActionServiceImpl) doFavoriteAction() error {
-
-	////获取用户的ID
-	//user,err := dal.QueryUserByToken(s.Ctx, s.Req.Token)
-	//if err!=nil{
-	//	log.Printf("获取不到用户信息：%v", err)
-	//	return err
-	//}
-
+	//获取用户的ID
+	user,err := dal.QueryUserByToken(s.Ctx, s.Req.Token)
+	if err!=nil{
+		log.Printf("获取不到用户信息：%v", err)
+		return err
+	}
+	//userId := s.Req.UserId
 	//点赞或取消点赞
-	err := dal.FavoriteAction(s.Ctx, s.userId, s.Req.VideoId)
+	err = dal.FavoriteAction(s.Ctx, user.UserId, s.Req.VideoId, s.Req.ActionType)
 	return err
 }
 
